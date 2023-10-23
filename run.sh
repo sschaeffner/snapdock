820967d8e37aecb8e19082f7b8b4b1ac47c561b5
Simon Schäffner <simon.schaeffner@googlemail.com>
2023-10-23 21:33:54 +0200

    run everything

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 mkdir /run/dbus
 ls -al /run
-dbus-daemon --system --nofork --nosyslog --print-address
-#avahi-daemon --no-drop-root --no-chroot --no-rlimits --daemonize
-#sleep infinity
+dbus-daemon --system --nosyslog --print-address
+avahi-daemon --no-drop-root --no-chroot --no-rlimits --daemonize
+/app/snapserver
